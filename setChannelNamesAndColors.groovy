e4aec4e4b817b836b8596f10034bc4165a350775
UniversalBuilder <yannick.krempp@gmail.com>
2020-09-18 09:28:30 +0200

    Create setChannelNamesAndColors.groovy
    A script to set the channel colors and names automatically

---
+++ setChannelNamesAndColors.groovy
@@ -0,0 +1,11 @@
+//requires QuPath 0.2.x and above
+
+//setting channel names
+setChannelNames("Nuclei", "NCC", "MR")
+
+//setting channel colors using RGB values
+setChannelColors(
+    getColorRGB(0, 0, 255),
+    getColorRGB(0, 255, 0),
+    getColorRGB(255, 0, 0),
+)
